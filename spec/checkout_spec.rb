53243ad785b64d1ef5a1a627c7925915ecd78c51
jarkyn <jarkyn@ymail.com>
2015-04-24 10:49:34 +0100

    tests scan method updates subtotal

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -20,11 +20,18 @@ end
 describe Checkout do
 
 	describe "#scan" do
-		it "adds item to the bill" do
+		it "adds item to the subtotal" do
 			checkout = Checkout.new
 			item = Item.new("A", 50)
 			checkout.scan(item)
 			expect(checkout.subtotal).to eq(50)
+		end
+
+		it "keeps adding items to the subtotal" do
+			checkout = Checkout.new
+			checkout.scan(Item.new("A", 50))
+			checkout.scan(Item.new("B", 30))
+			expect(checkout.subtotal).to eq(80)
 		end
 
 	end
